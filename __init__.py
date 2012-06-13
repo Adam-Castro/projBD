731f9abe0b6178a2c08a2661936ebc1e3fec69ff
Faray <faray@uff>
2012-06-13 15:11:34 -0300

    Modificacao no endereco e etc

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+try:
+    import gtk
+except:
+    print("GTK Not Availible")
+    sys.exit(1)
+
+class TesteGui:
+	
+	
--- banco/endereco.py
+++ banco/endereco.py
@@ -1,11 +1,30 @@
 try:
-  import MySQLdb
+	import MySQLdb
 except:
-  print("MySQLdb Not Available")
+	print("MySQLdb Not Available")
 
 class endereco:
-  def __init__(self):
-  def insert(self):
-  def remove(self):
-  def altera(self):
+	def __init__(self):
+		self.newtork = "localhost"
+		self.user = "filipe"
+		self.passw = "123456"
+		self.bd = "teste"
+		self.db = MySQLdb.connect(self.newtork,self.user,self.passw,self.bd)
+	
+	def insert(self,self.rua,self.numero,self.bairro,self.cidade,self.estado,self.cep):
+		self.cursor = self.db.cursor()
+		self.sql = "INSERT INTO endereco (\
+                    rua,numero,bairro,cidade,estado,cep) VALUES (\
+                    '%s',%s,'%s','%s','%s',%s)"%\
+                    (self.rua,self.numero,self.bairro,self.cidade,self.estado,self.cep)
+        try:
+             self.cursor.execute(self.sql)
+             self.db.commit()       
+             print 'deu commit na tabela endereco'     
+		except:
+			self.db.rollback
+            print 'db rollback tabela endereco'
+        self.db.close()
+	def remove(self):
+	def altera(self):
 
