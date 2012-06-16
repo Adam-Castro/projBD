09840c2c63e7bf6fb10b79d126d000cc68ba57a6
Faray <faray@uff>
2012-06-16 19:40:15 -0300

    Script endereco modificado

--- __init__.py
+++ __init__.py
@@ -1,17 +1,26 @@
 import sys
 import os
 
-try:
-    import pygtk
-    pygtk.require("2.0")
-except:
-    pass
-try:
-    import gtk
-except:
-    print("GTK Not Availible")
-    sys.exit(1)
+#try:
+#    import pygtk
+#    pygtk.require("2.0")
+#except:
+#    pass
+#try:
+#    import gtk
+#except:
+#    print("GTK Not Availible")
+#    sys.exit(1)
 
-class TesteGui:
+from banco.endereco import *
+
+class principal:
 	
+	def __init__(self):
+		self.end = endereco()
+		#self.end.create()
+		self.end.insert('bronze','227','ouro verde','rio das ostras','rj','28890000')
+		self.end.close()
+if __name__ == '__main__':
+	principal()
 	
--- banco/endereco.py
+++ banco/endereco.py
@@ -1,30 +1,46 @@
 try:
-	import MySQLdb
+    import MySQLdb
 except:
-	print("MySQLdb Not Available")
+    print("MySQLdb Not Available")
 
 class endereco:
-	def __init__(self):
-		self.newtork = "localhost"
-		self.user = "filipe"
-		self.passw = "123456"
-		self.bd = "teste"
-		self.db = MySQLdb.connect(self.newtork,self.user,self.passw,self.bd)
-	
-	def insert(self,self.rua,self.numero,self.bairro,self.cidade,self.estado,self.cep):
-		self.cursor = self.db.cursor()
-		self.sql = "INSERT INTO endereco (\
-                    rua,numero,bairro,cidade,estado,cep) VALUES (\
-                    '%s',%s,'%s','%s','%s',%s)"%\
-                    (self.rua,self.numero,self.bairro,self.cidade,self.estado,self.cep)
-        try:
-             self.cursor.execute(self.sql)
-             self.db.commit()       
-             print 'deu commit na tabela endereco'     
-		except:
-			self.db.rollback
-            print 'db rollback tabela endereco'
-        self.db.close()
-	def remove(self):
-	def altera(self):
+    def __init__(self):
+        self.newtork = "localhost"
+        self.user = "filipe"
+        self.passw = "123456"
+        self.bd = "teste"
+        self.conecta()
 
+    def create(self):
+        self.cursor = self.db.cursor()
+        self.sql = """
+            CREATE TABLE endereco_tbl(
+            identificador int(4) AUTO_INCREMENT,
+            rua varchar(30) NOT NULL,
+            numero int(4),
+            bairro varchar(30) NOT NULL,
+            cidade varchar(30) NOT NULL,
+            estado varchar(2) NOT NULL,
+            cep varchar(8),
+            PRIMARY KEY (identificador))"""
+        self.cursor.execute(self.sql)
+
+    def insert(self,rua,numero,bairro,cidade,estado,cep):
+        self.cursor = self.db.cursor()		
+        self.sql = """INSERT INTO endereco_tbl (identificador,rua,numero,bairro,cidade,estado,cep) 
+            VALUES ('','%s',%s,'%s','%s','%s','%s')""" % (rua,numero,bairro,cidade,estado,cep)
+        try:
+            self.cursor.execute(self.sql)
+            self.db.commit()
+            print 'deu commit na tabela endereco'
+        except:
+            self.db.rollback()
+            print 'db rollback tabela endereco'
+
+#def remove(self):
+#def altera(self):
+    def conecta(self):
+        self.db = MySQLdb.connect(self.newtork,self.user,self.passw,self.bd)
+
+    def close(self):
+        self.db.close()
